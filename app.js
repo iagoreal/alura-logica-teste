0e2c620d86b9c5bc6065eccbd2a7b892143bf9ba
Iago <iagovreal@gmail.com>
2025-02-18 17:46:41 -0300

    create function of add friend to list

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigosParaSortear = [];
+let nomeAmigo
+
+
+function adicionarAmigo() {
+    nomeAmigo = document.querySelector(".input-name").value;
+
+    if (nomeAmigo == "") {
+        alert("digite um nome válido");
+
+    } else {
+        listaAmigosParaSortear.push(nomeAmigo);
+        const container = document.getElementById("listaAmigos");
+        const li = document.createElement("li");
+        li.textContent = nomeAmigo;
+        container.appendChild(li);
+        document.querySelector(".input-name").value = "";
+        console.log(listaAmigosParaSortear);
+
+    }
+}
+
+
